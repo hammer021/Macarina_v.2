ae4755b931e0a263a4e00e5ad290a72abb6cfc9b
baidowii <baidowi.jr.10@gmail.com>
2020-04-30 20:09:03 +0700

    Home
    No error

--- Web/application/controllers/Home.php
+++ Web/application/controllers/Home.php
@@ -3,13 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	function __construct(){
-		parent::__construct();
+	//function __construct(){
+	//	parent::__construct();
 	
-		if($this->session->userdata('status') != "login"){
-			redirect(site_url("Login"));
-		}
-	}
+	//	if($this->session->userdata('status') != "login"){
+	//		redirect(site_url("Login"));
+	//	}
+	//}
 	
 	public function index()
 	{
---
+++ Web/application/views/utemplate/footer.php
@@ -0,0 +1,54 @@
+
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+      	<div class="row">
+      		<div class="mouse">
+						<a href="#" class="mouse-icon">
+							<div class="mouse-wheel"><span class="ion-ios-arrow-up"></span></div>
+						</a>
+					</div>
+      	</div>
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Kenapa Macarina?</h2>
+              <p>Nama Macarina yang dipakai tersebut didapat dari singkatan "Makaroni Nagih", karena pada saat mencoba macaroni pertama kali kepada orang-orang di sekitar, mereka berpendapat bahwa makanan ringannya tersebut membuat mereka merasa nagih atau ketagihan untuk mencicipi terus menerus.</p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate"><a href="https://www.facebook.com/macarina.id/"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.instagram.com/macarina.id/"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-5">
+              <h2 class="ftco-heading-2">Menu</h2>
+              <ul class="list-unstyled">
+                <li><a href="shop.php" class="py-2 d-block">Shop</a></li>
+                <li><a href="profilperusahaan.php" class="py-2 d-block">Tentang</a></li>
+                <li><a href="contact.php" class="py-2 d-block">Kontak kami</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Tanyakan pada kami</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="icon icon-map-marker"></span><span class="text">Jl. Sriwijaya XX No.11 Karangrejo - Sumbersari, JEMBER</span></li>
+	                <li><a href="#"><span class="icon icon-phone"></span><span class="text">081 217 757 777</span></a></li>
+	                <li><a href="#"><span class="icon icon-envelope"></span><span class="text">macarina.official@gmail.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+          <div style="color:#27262e;line-height:1.2;padding:15px 15px 15px 25px;font-size:12px">
+          <a href="/" alt="macarina.com" class="nuxt-link-exact-active nuxt-link-active" style="font-size:14px;color:#eb1c24">Macarina.com </a>
+          <br>Graha Macarina, CV.Macarindo Berkah Group
+          
+          </div>
+        </div>
+      </div>
+    </footer>
