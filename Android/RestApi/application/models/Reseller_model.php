0ee4b1ed71852cf4021e4da02ff90a41f99d25d4
Luqman <e41180965@student.polije.ac.id>
2020-05-06 16:48:09 +0700

    Restapi Android

---
+++ Android/RestApi/application/models/Reseller_model.php
@@ -0,0 +1,37 @@
+<?php 
+ 
+class Reseller_model extends CI_Model {
+    protected $akun_reseller = 'reseller';
+
+    public function insert_akun(array $data) {
+        $this->db->insert($this->akun_reseller, $data);
+        return $this->db->insert_id();
+    }
+
+    public function akun_login($email, $password) 
+    {
+        $this->db->where('reseller', $email);
+        $q = $this->db->get($this->akun_reseller);
+
+        if($q->num_rows()) {
+            $akun_pass = $q->row('password');
+            if(md5($password) === $akun_pass) {
+                return $q->row();
+            }
+            return FALSE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function getReseller($id = null)
+    {
+        if($id === null){
+            return $this->db->get('reseller')->result_array();
+        } else {
+            return $this->db->get_where('reseller', ['id_reseller' => $id])->result_array();
+        }
+        
+    }
+
+}
