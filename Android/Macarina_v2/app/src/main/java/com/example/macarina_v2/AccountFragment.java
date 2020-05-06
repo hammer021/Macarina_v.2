23e6346a2ddc7d5eb60d538bd635475708c2cc76
Luqman <e41180965@student.polije.ac.id>
2020-05-06 09:11:18 +0700

    done home

--- Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/AccountFragment.java
+++ Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/AccountFragment.java
@@ -15,7 +15,8 @@ public class AccountFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_account_fragment, container, false);
+        View v = inflater.inflate(R.layout.activity_account_fragment, container, false);
         //        return super.onCreateView(inflater, container, savedInstanceState);
+        return v;
     }
 }
