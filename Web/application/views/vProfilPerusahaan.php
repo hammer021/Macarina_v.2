6a6c21d0465a807b8b749d4dd3faf4e13a7119af
eilham <e41180954@student.polije.ac.id>
2020-05-06 18:54:13 +0700

    include css and js
    include css & js  user

--- Web/application/views/vProfilPerusahaan.php
+++ Web/application/views/vProfilPerusahaan.php
@@ -2,7 +2,7 @@
     $this->load->view("utemplate/header");
     ?>
 
-    <div class="hero-wrap hero-bread" style="background-image: url('images/edit.jpg');">
+    <div class="hero-wrap hero-bread" style="background-image: url('<?php base_url("images/edit.jpg") ?>');">
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
@@ -144,28 +144,3 @@
     $this->load->view("utemplate/footer");
     ?>
   
-
-  <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
-
-
-  <script src="<?php echo base_url('js/jquery.min.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery-migrate-3.0.1.min.js')?>"></script>
-  <script src="<?php echo base_url('js/popper.min.js')?>"></script>
-  <script src="<?php echo base_url('js/bootstrap.min.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery.easing.1.3.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery.waypoints.min.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery.stellar.min.js')?>"></script>
-  <script src="<?php echo base_url('js/owl.carousel.min.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery.magnific-popup.min.js')?>"></script>
-  <script src="<?php echo base_url('js/aos.js')?>"></script>
-  <script src="<?php echo base_url('js/jquery.animateNumber.min.js')?>"></script>
-  <script src="<?php echo base_url('js/bootstrap-datepicker.js')?>"></script>
-  <script src="<?php echo base_url('js/scrollax.min.js')?>"></script>
-  <script src="<?php echo base_url('https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false')?>"></script>
-  <script src="<?php echo base_url('js/google-map.js')?>"></script>
-  <script src="<?php echo base_url('js/main.js')?>"></script>
- 
-    
-  </body>
-</html>
