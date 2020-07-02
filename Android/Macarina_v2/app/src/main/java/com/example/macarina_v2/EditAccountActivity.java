e9c56121e204a70e03de55796d5e267bed10fb42
Luqman <e41180965@student.polije.ac.id>
2020-07-02 23:42:19 +0700

    Transaksi

--- Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/EditAccountActivity.java
+++ Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/EditAccountActivity.java
@@ -1,24 +1,55 @@
 package com.example.macarina_v2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.ClientError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.macarina_v2.configfile.ServerApi;
 import com.example.macarina_v2.configfile.authdata;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditAccountActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
     authdata authdataa;
     RequestQueue queue;
     ArrayAdapter<String> adapter;
+
+    private final int IMG_REQUEST = 1;
+    boolean statusImage = false;
 
     String mIdReseller;
     String mNama;
@@ -32,6 +63,10 @@ public class EditAccountActivity extends AppCompatActivity {
     EditText txtNama, txtTelepon, txtEmail, txtAlamat, txtNoKTP;
     TextView txtPathScanKTP, txtPathPasFoto;
     Button btnUpdateProfil, btnScanKTP, btnPasFoto;
+    Bitmap bitmapProfil, BitmapProfil2;
+
+    String selected;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,14 +79,53 @@ public class EditAccountActivity extends AppCompatActivity {
         mIdReseller = authdataa.getKodeUser();
 
         initWidgetId();
+        btnUpdateProfil.setEnabled(false);
+        loadProfil();
+
+        btnScanKTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selected = "KTP";
+                selectImage();
+            }
+        });
+        btnPasFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selected = "PAS_FOTO";
+                selectImage();
+            }
+        });
+
+        btnUpdateProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateTextInput(txtNoKTP, "Nik harus diisi!") &
+                        validateTextInput(txtNama, "Nama harus diisi!") &
+                        validateTextInput(txtAlamat, "Alamat harus diisi!") &
+                        validateTextInput(txtTelepon, "Telepon harus diisi!") &
+                        validateTextInput(txtEmail, "Email harus diisi!")) {
+
+                    updateProfil();
+
+                    Intent main = new Intent(EditAccountActivity.this, MainActivity.class);
+                    startActivity(main);
+                    finish();
+
+                } else {
+                    Snackbar.make(findViewById(R.id.activity_edit_account), "Data diri belum terpenuhi", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-//    private void selectImage() {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(intent.ACTION_GET_CONTENT);
-//        startActivityForResult(intent, IMG_REQUEST);
-//    }
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMG_REQUEST);
+//        startActivityForResult(intent, IMG_REQUEST2);
+    }
 
     private void initWidgetId() {
         btnUpdateProfil = findViewById(R.id.btnupdateakun);
@@ -64,5 +138,183 @@ public class EditAccountActivity extends AppCompatActivity {
         txtAlamat = findViewById(R.id.edtxalamat);
         txtTelepon = findViewById(R.id.edtxno);
         txtEmail = findViewById(R.id.edtxtemail);
+    }
+
+    private boolean validateTextInput(EditText editText, String errorMessage) {
+        String input = editText.getText().toString().trim();
+
+        if (input.isEmpty()) {
+            editText.setError(errorMessage);
+            return false;
+        } else {
+            editText.setError(null);
+            return true;
+        }
+    }
+
+    //    konversi gambar menjadi string
+    private String imageToString(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, byteArrayOutputStream);
+        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void loadProfil() {
+        progressDialog.setMessage("Sedang Memperbarui Data...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String url = ServerApi.URL_USER + mIdReseller;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String status = jsonObject.getString("status");
+
+                    if (status.equals("false")) {
+                        String message = jsonObject.getString("message");
+                        Snackbar.make(findViewById(R.id.activity_edit_account), message, Snackbar.LENGTH_LONG).show();
+                    } else {
+                        JSONArray data = jsonObject.getJSONArray("data");
+                        JSONObject dataUser = data.getJSONObject(0);
+
+                        mNama = dataUser.getString("nama_reseller");
+                        mAlamat = dataUser.getString("alamat");
+                        mTelepon = dataUser.getString("no_tlp");
+                        mScanKTP = dataUser.getString("scan_ktp");
+                        mNoKTP = dataUser.getString("no_ktp");
+                        mEmail = dataUser.getString("email");
+                        mPasFoto = dataUser.getString("pas_foto");
+
+                        txtNama.setText(mNama);
+                        txtAlamat.setText(mAlamat);
+                        txtTelepon.setText(mTelepon);
+                        txtNoKTP.setText(mNoKTP);
+                        txtEmail.setText(mEmail);
+
+                        btnUpdateProfil.setEnabled(true);
+                    }
+                } catch (Exception e) {
+                    Snackbar.make(findViewById(R.id.activity_edit_account), e.toString(), Snackbar.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                String message = "Terjadi error. Coba beberapa saat lagi.";
+
+                if (error instanceof NetworkError) {
+                    message = "Tidak dapat terhubung ke internet. Harap periksa koneksi anda.";
+                } else if (error instanceof AuthFailureError) {
+                    message = "Gagal login. Harap periksa email dan password anda.";
+                } else if (error instanceof ClientError) {
+                    message = "Gagal login. Harap periksa email dan password anda.";
+                } else if (error instanceof NoConnectionError) {
+                    message = "Tidak ada koneksi internet. Harap periksa koneksi anda.";
+                } else if (error instanceof TimeoutError) {
+                    message = "Connection Time Out. Harap periksa koneksi anda.";
+                }
+
+                Snackbar.make(findViewById(R.id.activity_edit_account), message, Snackbar.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void updateProfil() {
+        progressDialog.setMessage("Sedang Memperbarui Data...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String url = ServerApi.URL_PUT_USER;
+
+        StringRequest updateRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String message = jsonObject.getString("message");
+
+                    Snackbar.make(findViewById(R.id.activity_edit_account), message, Snackbar.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                String message = "Terjadi error. Coba beberapa saat lagi.";
+
+                if (error instanceof NetworkError) {
+                    message = "Tidak dapat terhubung ke internet. Harap periksa koneksi anda.";
+                } else if (error instanceof AuthFailureError) {
+                    message = "Gagal login. Harap periksa email dan password anda.";
+                } else if (error instanceof ClientError) {
+                    message = "Gagal login. Harap periksa email dan password anda.";
+                } else if (error instanceof NoConnectionError) {
+                    message = "Tidak ada koneksi internet. Harap periksa koneksi anda.";
+                } else if (error instanceof TimeoutError) {
+                    message = "Connection Time Out. Harap periksa koneksi anda.";
+                }
+
+                Snackbar.make(findViewById(R.id.activity_edit_account), message, Snackbar.LENGTH_LONG).show();
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+                params.put("id_reseller", mIdReseller);
+                params.put("nama_reseller", txtNama.getText().toString().trim());
+                params.put("alamat", txtAlamat.getText().toString().trim());
+                params.put("no_tlp", txtTelepon.getText().toString().trim());
+                params.put("scan_ktp", imageToString(bitmapProfil));
+                params.put("no_ktp", txtNoKTP.getText().toString().trim());
+                params.put("email", txtEmail.getText().toString().trim());
+                params.put("pas_foto", imageToString(BitmapProfil2));
+
+                return params;
+            }
+        };
+
+        queue.add(updateRequest);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMG_REQUEST && resultCode == RESULT_OK && data != null) {
+//            mengambil alamat file gambar
+            Uri path = data.getData();
+//            Uri path2 = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(path);
+//                InputStream inputStream2 = getContentResolver().openInputStream(path2);
+                String pathGambar = path.getPath();
+//                String pathGambar2 = path2.getPath();
+
+                if(selected.equals("KTP")){
+                    bitmapProfil = BitmapFactory.decodeStream(inputStream);
+                    txtPathScanKTP.setText(pathGambar);
+                } else {
+                    BitmapProfil2 = BitmapFactory.decodeStream(inputStream);
+                    txtPathPasFoto.setText(pathGambar);
+                }
+                statusImage = true;
+
+            } catch (FileNotFoundException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/MapsActivity.java
+++ Android/Macarina_v2/app/src/main/java/com/example/macarina_v2/MapsActivity.java
@@ -1,63 +1,27 @@
 package com.example.macarina_v2;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
+
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private LatLng Outlet_Karimata = new LatLng(-8.174065, 113.719192);
-    private LatLng Outlet_Kalimantan = new LatLng(-8.166172, 113.711811);
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-    private Marker mCurrLocationMarker;
-    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Memuat SupportMapFragment dan memberi notifikasi saat telah siap.
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        mMap.addMarker(new MarkerOptions()
-                .position(Outlet_Kalimantan)
-                .icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                .title("Outlet Kalimantan"));
-
-        mMap.addMarker(new MarkerOptions()
-                .position(Outlet_Karimata)
-                .icon(BitmapDescriptorFactory
-                        .defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                .title("Outlet Karimata"));
     }
 
     /**
@@ -73,113 +37,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //Memulai Google Play Services
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                buildGoogleApiClient();
-                mMap.setMyLocationEnabled(true);
-            }
-        }
-        else {
-            buildGoogleApiClient();
-            mMap.setMyLocationEnabled(true);
-        }
-    }
+        // Add a marker in Sydney and move the camera
+        LatLng mapkarimata = new LatLng(-8.174065, 113.719192);
+        mMap.addMarker(new MarkerOptions().position(mapkarimata).title("Outlet Karimata"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(mapkarimata));
 
-    private void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mLastLocation = location;
-        if (mCurrLocationMarker != null) {
-            mCurrLocationMarker.remove();
-        }
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-
-        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(latLng.latitude, latLng.longitude)).zoom(16).build();
-
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-        //menghentikan pembaruan lokasi
-        if (mGoogleApiClient != null) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-        }
-    }
-
-    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    public boolean checkLocationPermission(){
-        if (ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // Izin diberikan.
-                    if (ContextCompat.checkSelfPermission(this,
-                            android.Manifest.permission.ACCESS_FINE_LOCATION)
-                            == PackageManager.PERMISSION_GRANTED) {
-
-                        if (mGoogleApiClient == null) {
-                            buildGoogleApiClient();
-                        }
-                        mMap.setMyLocationEnabled(true);
-                    }
-
-                } else {
-
-                    // Izin ditolak.
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-        }
+        LatLng mapkalimantan = new LatLng(-8.166172, 113.711811);
+        mMap.addMarker(new MarkerOptions().position(mapkalimantan).title("Outlet Kalimantan"));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(mapkalimantan));
     }
 }
